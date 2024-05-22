caef3197dc1e0553379d71ebd48ada5cb8bf4375
Kalink52 <micah.cox52@gmail.com>
2024-05-22 10:58:13 -0400

    Added lyrics ovh and tested usablity

---
+++ assets/js/lyrics.js
@@ -0,0 +1,19 @@
+//not linked to html
+//isn't being ran
+
+//lyrics api testing variable
+// any spaces must be converted into dashes ie. -
+let artist = 'metallica'
+let title = 'one'
+
+function getLyrics() {
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    .then(function (response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data)
+
+    } )
+}
