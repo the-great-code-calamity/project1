7f83a8480f3435c6a2a952d755a342bb2be5246a
Daniel Hudson <LuckyLlama72@gmail.com>
2024-05-29 13:17:48 -0400

    Lyrics, artist and title now display on HTML page.

--- assets/js/lyrics.js
+++ assets/js/lyrics.js
@@ -1,25 +1,48 @@
 //not linked to html
 //isn't being ran
 
-//lyrics api testing variable
-// any spaces must be converted into dashes ie. -
-// let artist = 'Frizk'
-// let title = 'ALL MY FELLAS'
+//lyrics api testing variable (Do not change it, I will be very sad. -DH)
+// any spaces must be converted into dashes ie. - (Done - DH)
+let artist = 'FRIZK'
+let title = 'ALL MY FELLAS'
 
-
-export function getLyrics( artist, title) {
-    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+// Remember to add export when done testing
+export function getLyrics(artist, title) {
+    let dashArtist = artist.replace(/\s+/g, '-');
+    let dashTitle = title.replace(/\s+/g, '-');
+    fetch(`https://api.lyrics.ovh/v1/${dashArtist}/${dashTitle}`)
     .then(function (response){
         if (!response.ok){
-            return ('Lyrics Unavailable!');
+            // console.log("Bruh you goofy.")
+            // If someone wanna fix this be my guest, this works but could be better.
+            let data = []
+            data.lyrics = 'Lyrics Unavailable!'
+            return data;
         }
         console.log(response)
         return response.json();
     })
     .then(function (data){
-        console.log(data)
+        console.log(data);
+        // Could be better, but it'll do for now.
+        lyricsTarget = document.getElementById('lyricsContainer');
+        artistTarget = document.getElementById('artistContainer');
+        titleTarget = document.getElementById('titleContainer');
 
+        // Fix lyrics to look more appealing
+        let lyrics1 = data.lyrics.replace(/\r\n/g , "\r\n");
+        console.log(lyrics1);
+        let lyrics2 = lyrics1.replace(/\n\n/g , '\n\n');
+        console.log(lyrics2);
+        let newLyrics = lyrics2.replace(/\n/g , '\n');
+        console.log(newLyrics);
+        // Does da ting, what else can I say?
+        lyricsTarget.textContent = newLyrics;
+        artistTarget.textContent = artist;
+        titleTarget.textContent = title;
     } )
 }
 
-getLyrics(artist, title);
+
+
+// getLyrics(artist, title);
