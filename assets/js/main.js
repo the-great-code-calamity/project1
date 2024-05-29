d0948611dafeb9c46c8815fb46f686b51971505e
Jorge Guzman <jorgeguzman@Jorges-iMac.local>
2024-05-28 21:45:17 -0400

    started js for modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 /* The main logic controller for the application which
    imports from additional modules/libraries as needed
 */
+document.getElementById('searchBtn').addEventListener('click', function() {
+  
+   const artistName = document.getElementById('artistNameInput').value
+   const songName = document.getElementById('songNameInput').value
+   console.log('Artist Name:', artistName);
+   console.log('Song Name:', songName);
+})
+
+
+
 
 /* make use of the record start/stop from the audio library */
 import { recordStart, recordStop } from "./audio.js";
