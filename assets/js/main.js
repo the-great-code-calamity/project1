7350a4e6522e1ae47c668280e814d692a61ecc8f
Jorge Guzman <jorgeguzman@Jorges-iMac.local>
2024-05-30 19:47:39 -0400

    removed console.log

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,7 @@
 document.getElementById("searchBtn").addEventListener("click", function () {
   let artistName = document.getElementById("artistNameInput").value;
   let songName = document.getElementById("songNameInput").value;
-  console.log("Artist Name:", artistName);
-  console.log("Song Name:", songName);
-
+ 
   const artistSong = document.getElementById("artist-song");
   const button = document.createElement("button");
   button.textContent = `${artistName} - ${songName}`;
