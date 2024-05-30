d1ad0c6079388427566599bba36a853413926040
Jorge Guzman <jorgeguzman@Jorges-iMac.local>
2024-05-29 22:27:21 -0400

    added close modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 /* The main logic controller for the application which
    imports from additional modules/libraries as needed
 */
+
+
+//input items in modal and save to local storage
 document.getElementById('searchBtn').addEventListener('click', function() {
-  
+
    let artistName = document.getElementById('artistNameInput').value
    let songName = document.getElementById('songNameInput').value
    console.log('Artist Name:', artistName);
@@ -18,8 +21,22 @@ document.getElementById('searchBtn').addEventListener('click', function() {
   
    localStorage.setItem('songData', JSON.stringify(firstSongInfo));
 
-   
+   const modal = document.getElementById('my_modal_3');
+   modal.close();
+
 });
+//get items from local storage and display on window
+const data = localStorage.getItem('songData');
+const parsedData = JSON.parse(data);
+const artistSong = document.getElementById('artist-song');
+
+for (let i = 0; i < parsedData.length; i++) {
+   const button = document.createElement('button');
+   button.textContent = `${parsedData[i].artistName} - ${parsedData[i].songName}`;
+   button.classList.add('btn', 'btn-ghost');
+   artistSong.appendChild(button);
+};
+
 
 
 
